06d04e3253c434963bbc2abbb2d030768b53fbc1
Serabass <serabass565@gmail.com>
2026-04-06 01:32:26 +0500

    1

--- gen.ps1
+++ gen.ps1
@@ -1,2 +1,5 @@
 docker run --rm -v "e:\.dev\.vibecoding\reknow-dda:/src" -w /src mcr.microsoft.com/dotnet/sdk:8.0 dotnet run --project src/Reac/Reac.csproj -c Release -- export-html -p /src
 docker --host tcp://192.168.88.100:32375 buildx bake site --load --push
+
+kubectl --kubeconfig E:\.dev\.vibecoding\infra-k3s-remake2\kubeconfig rollout restart deployment reac -n reac
+kubectl --kubeconfig E:\.dev\.vibecoding\infra-k3s-remake2\kubeconfig rollout status deploy/reac -n reac
